f2cdd27edf38b2cbf018bb09de042eb576f75a13
krizopras <krizopras14@gmail.com>
2025-08-27 17:24:13 +0300

    Update bot_bist.py

--- bot_bist.py
+++ bot_bist.py
@@ -165,7 +165,7 @@ STOCHRSI_D = int(os.getenv("STOCHRSI_D", "3"))
 MA_SHORT = int(os.getenv("MA_SHORT", "50"))
 MA_LONG = int(os.getenv("MA_LONG", "200"))
 # Çok güçlü sinyaller için minimum puan eşiği yükseltildi
-MIN_SIGNAL_SCORE = float(os.getenv("MIN_SIGNAL_SCORE", "15.0")) 
+MIN_SIGNAL_SCORE = float(os.getenv("MIN_SIGNAL_SCORE", "20.0")) 
 
 LAST_SCAN_TIME: Optional[dt.datetime] = None
 START_TIME = time.time()
